45adb217957a2e11cb8a925011bba8f10a68bc86
mandrakey <mandrakey@lavabit.com>
2013-04-25 09:06:43 +0200

    Begin adding license and doxygen comments.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,28 @@
+/*
+ * Copyright (C) 2013  Maurice Bleuel (mandrakey@lavabit.com)
+ *
+ * This file is part of GradeCalc.
+ *
+ * GradeCalc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GradeCalc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GradeCalc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+  * Application entry point.
+  * Initializes ProgramOptions and shows MainWindow.
+  * @file main.cpp
+  */
+
 #include <QApplication>
 #include <QtXml/QDomDocument>
 #include <QFile>
--- model/application.h
+++ model/application.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2013  Maurice Bleuel (mandrakey@lavabit.com)
+ *
+ * This file is part of GradeCalc.
+ *
+ * GradeCalc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GradeCalc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GradeCalc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
@@ -11,6 +30,11 @@ using std::endl;
 
 #include "institution.h"
 
+/**
+ * Contains application-wide data.
+ * Implemented as a monostate object.
+ * @ingroup model
+ */
 class Application
 {
 public:
@@ -22,11 +46,22 @@ public:
     const QString copyright() const;
     const QString license() const;
 
+    /**
+     * Load the data.xml file containing available institutions, courses.
+     * @todo Allow change of file path?
+     */
     void loadDatabase();
+
+    /**
+     * Empty institutions list, delete all pointer targets.
+     */
     void cleanup();
 
 private:
+    /** Initialization (loading license text etc.) completed? */
     static bool INITIALIZED;
+
+    /** Contains a list of all loaded institutions. */
     static QList<Institution *> INSTITUTIONS;
 
     static const char *APPNAME;
